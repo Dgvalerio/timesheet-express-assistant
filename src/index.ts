d7f3ec91f191019d19a7ff5a48984fcbe7809ff3
dgvalerio <al.davigvalerio@gmail.com>
2022-02-10 19:52:43 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const sum = (number1: number, number2: number): number => number1 + number2;
+import express from 'express';
 
-console.log({ sum: sum(7, 5) });
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
